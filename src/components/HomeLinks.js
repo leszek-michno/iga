e7a360165c48cf8af4154499b137d49a05772a08
Leszek Michno <leszek.michno@gmail.com>
2023-02-04 20:59:05 +0100

    HomeLinks witout error

--- src/components/HomeLinks.js
+++ src/components/HomeLinks.js
@@ -11,12 +11,12 @@ const HomeLinks = () => {
     { name: "rysunki", path: "/draw", img: imgDr },
     { name: "digital", path: "/digital", img: imgD },
     { name: "szkice", path: "/sketch", img: imgS },
-    { name: "malarstwo", path: "/pictures", img: imgM },
+    { name: "malarstwo", path: "/pictures", img: imgM }
     // { name: "tatuaże", path: "/tatoo", img: },
   ];
-
+  
   const album = albums.map((item) => (
-    <Link path key={item.name} to={item.path}>
+    <Link key={item.name} to={item.path}>
       <figure>
         <figcaption>{item.name}</figcaption>
         <img src={item.img} alt="" />
@@ -28,7 +28,9 @@ const HomeLinks = () => {
     <>
       <h2>Albumy</h2>
       <div className="albums">
-        <div className="links">{album}</div>
+        <div className="links">
+          {album}
+          </div>
       </div>
     </>
   );
